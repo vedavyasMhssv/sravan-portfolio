5a9616d1b6f7e547172da480b7a6ab71a82bf392
vedavyas@angadiworldtech.com <vedavyas@angadiworldtech.com>
2024-03-31 03:36:24 +0530

    rating info added

--- src/components/RatingsAndClients/index.js
+++ src/components/RatingsAndClients/index.js
@@ -19,12 +19,94 @@ import ukd from "@/images/our-clients/ukd.png";
 import wdh from "@/images/our-clients/wdh.png";
 import wink from "@/images/our-clients/wink.png";
 
+import rating from "@/images/our-clients/rating.png";
+import stars from "@/images/our-clients/stars.png";
+
 function RatingsAndClients() {
+  const ratings = [
+    {
+      content:
+        "AWT did a great job in designing our website and they had a great team who has helped us all through the project from initial discussions to getting our website online. Sravan, Abhi and Dhivya worked seamlessly with our team. Expert's in what they do and strongly suggest everyone. Thank you.",
+      name: "Sudheer",
+      role: "web development",
+      rating: "5",
+    },
+    {
+      content:
+        "I want to thank you for giving me the opportunity to serve as a intern at Angadi World Technologies.I have gained valuable insight and increased my interest in pursuing a career.",
+      name: "Mary Madhuri",
+      role: "intern",
+      rating: "5",
+    },
+    {
+      content:
+        "Best place to get any kind of Web and Mobile Applications to be Developed",
+      name: "Rohan Gunai",
+      role: "development",
+      rating: "5",
+    },
+    {
+      content:
+        "For interns, this Angadi world Technologies company offers the best opportunity to gain valuable learning experiences.",
+      name: "Veluri Sai kusuma",
+      role: "intern",
+      rating: "5",
+    },
+  ];
   return (
     <div className="bg-white text-black py-10">
-      <div className="mt-5 mb-20  p-10 flex flex-wrap">
-        <div className="flex flex-col"></div>
-        <div className="flex flex-col"></div>
+      <div className="mt-5 mb-20  p-10 flex flex-wrap  md:max-w-[80%] mx-auto">
+        <div className="flex flex-col flex-1 px-10">
+          <p className="text-3xl font-bold leading-9 tracking-wide  text-black">
+            your satisfaction is my first priority. Delightful Testimonials from
+            Our Satisfied Clients!
+          </p>
+          <Image
+            src={rating}
+            alt="alpha"
+            height={400}
+            width={400}
+            unoptimized
+            className="mt-5"
+          />
+        </div>
+        <div className="flex flex-col flex-1 min-h-[50px] px-16  justify-between">
+          {ratings &&
+            ratings.map((rating, index) => {
+              return (
+                <div
+                  className="flex flex-col border-b border-b-black"
+                  key={index}
+                >
+                  <p className="text-sm font-semibold leading-tight text-justify">
+                    {rating.content}
+                  </p>
+                  <div className="flex my-1 justify-between items-center">
+                    <p>
+                      <span className="text-lg font-bold leading-snug">
+                        {rating.name}
+                      </span>{" "}
+                      -{" "}
+                      <span className="text-base font-medium leading-tight">
+                        {" "}
+                        {rating.role}
+                      </span>
+                    </p>
+                    <Image
+                      src={stars}
+                      alt="rating"
+                      height={100}
+                      width={100}
+                      style={{
+                        width: "75px",
+                        height: "14px",
+                      }}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
       <div className="">
         <p className="m-w-fit text-xl font-bold leading-tight tracking-tighter uppercase text-center mb-10">

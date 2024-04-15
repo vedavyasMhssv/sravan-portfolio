60d42f4302e4bae8e1944b0dec56e569156bc4c1
Santoshalimkar <santosh.sa4d@gmail.com>
2024-04-15 19:30:06 +0530

    changes added

--- src/components/SocialFeeds/index.js
+++ src/components/SocialFeeds/index.js
@@ -133,7 +133,7 @@ function SocialFeeds() {
           );
         })}
       </div>
-      <div className="w-11/12 mx-auto mt-10 px-10">
+      <div className="w-11/12 md:w-[95%] mx-auto mt-10 px-10">
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 500: 2, 800: 3 }}>
           <Masonry gutter="10px">
             {mergedData.slice(0, numPosts).map((value, index) => {
@@ -154,7 +154,7 @@ function SocialFeeds() {
                 </div>
                 <div
                   key={index}
-                  className="px-2 py-4 mx-auto hidden md:flex w-auto border border-black max-h-fit rounded-lg  flex-col overflow-hidden  text-black"
+                  className="px-2 py-4 mx-auto hidden md:flex   w-auto border border-black max-h-fit rounded-lg  flex-col overflow-hidden  text-black"
                   style={{
                     minHeight: "fit-content",
                     boxShadow: "5px 6px 0px 0px #000000B0",
